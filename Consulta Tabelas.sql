64b7ffb8ead5594c45b926dc0e2230124deb1e83
Curatola <olavo.curatola@gmail.com>
2017-06-01 11:37:58 -0300

    Add files via upload

---
+++ Consulta Tabelas.sql
@@ -0,0 +1,29 @@
+SELECT * FROM public.andar
+ORDER BY id DESC
+
+SELECT * FROM public.equipamento
+ORDER BY id ASC 
+
+SELECT * FROM public.local
+ORDER BY id ASC 
+
+SELECT * FROM public.ocorrencia
+ORDER BY id ASC 
+
+SELECT * FROM public.ordem_de_servico
+ORDER BY id ASC 
+
+SELECT * FROM public.padrao
+ORDER BY id ASC 
+
+SELECT * FROM public.perfil
+ORDER BY id ASC 
+
+SELECT * FROM public.plano
+ORDER BY id ASC 
+
+SELECT * FROM public.status
+ORDER BY id ASC 
+
+SELECT * FROM public.usuario
+ORDER BY matricula ASC, id ASC 
